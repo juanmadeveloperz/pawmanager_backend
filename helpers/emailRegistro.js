0600e4e58bcef9c3efbed25810a7b9b7aa655502
Juan Manuel E.P. <juanmaesppast@gmail.com>
2025-05-18 14:11:04 +0200

    first commit

---
+++ helpers/emailRegistro.js
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer';
+
+const emailRegistro = async (datos) => {
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    const { email, nombre, token } = datos;
+
+    // Enviar el email
+    const info = await transport.sendMail({
+        from: "APV - Administrador de Pacientes de Veterinaria",
+        to: email,
+        subject: "Confirma tu cuenta en APV",
+        text: "Confirma tu cuenta en APV",
+        html: `<p>Hola ${nombre}, comprueba tu cuenta en APV.</p>
+            <p>Tu cuenta ya está lista, solo debes confirmarla en el siguiente enlace:
+            <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Confirmar cuenta</a> </p>
+            
+            <p>Si no creaste esta cuenta, puedes ignorar este mensaje</p>`,
+    });
+
+    console.log("Mensaje enviado: %s", info.messageId);
+
+}
+
+export default emailRegistro;
---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import conectarDB from './config/db.js';
+import veterinarioRoutes from './routes/veterinarioRoutes.js';
+import pacienteRoutes from './routes/pacienteRoutes.js';
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+conectarDB();
+
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 ) {
+            // El origen del request está permitido
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+}
+
+app.use(cors(corsOptions));
+
+
+
+app.use("/api/veterinarios", veterinarioRoutes);
+app.use("/api/pacientes", pacienteRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en: ${PORT}`);
+});
